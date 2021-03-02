694b9a756f04f64c56d3e593ca32a3f0fc03fa37
Aurorehey <48832067+Aurorehey@users.noreply.github.com>
2021-03-02 23:42:14 +0100

    intégration du menu de présentation

---
+++ Assets/_EcranDePrésentation/Passe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Passe : MonoBehaviour
+{
+    public string jepasse;
+
+    public void mapagedeprésentation()
+    {
+        SceneManager.LoadScene(jepasse);
+    }
+}
+
