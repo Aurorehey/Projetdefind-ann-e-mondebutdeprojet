a734ed03aabf836138f992d1b6a9dcc8b9dbf738
Aurorehey <48832067+Aurorehey@users.noreply.github.com>
2021-03-10 00:27:18 +0100

    travail sur éteindre et allumer la lampe torche

---
+++ Assets/Script/Flashlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject Spotlight;
+    public bool isOn = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+            isOn = !isOn;
+        { 
+            if (isOn == true)
+            {
+                Spotlight.SetActive(true);
+            }
+            if (isOn == false)
+            {
+                Spotlight.SetActive(false);
+            }
+        }
+        
+
+
+        
+        
+    }
+}
